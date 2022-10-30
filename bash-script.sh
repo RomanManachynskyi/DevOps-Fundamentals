15ce9affbcbe4cbd38f5e26962a50923c69d9d2e
RomanManachynskyi <roman.manachynskyi@gmail.com>
2022-10-30 21:36:16 +0200

    Update: Modify bash-script.sh

--- bash-script.sh
+++ bash-script.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+echo "Start of the script"
+echo " "
+
+echo "Put down file name"
 read FILENAME
 
 while read line;
 do
      echo "$line"
 done < $FILENAME
+
+echo "Completion of the script"
